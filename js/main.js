04cadb812487ad75b23b9e2a4ef9b883eb47c3a6
Anecia <stanaszek.aneta@gmail.com>
2017-06-01 20:14:38 +0200

    zoom

--- js/main.js
+++ js/main.js
@@ -1,66 +1,23 @@
-"use strict";
+$(function(){
+	
 
-$(document).ready(function () {
+	
+	$('a[href*="#"]:not([href="#"])').click(function(){
+		$('body').animate( { scrollTop: $(this.hash).offset().top - 50 }, 500 );
+	});
 
-
-    $('.main-nav-link').on('click', function (e) {
-        e.preventDefault();
-
-        var target = $(this.hash);
-        var offset = target.offset().top - 30;
-
-        $('html, body').animate({
-            scrollTop: offset
-        }, 1000);
-        
-    
-        if (window.innerWidth <= 768) {
-            $('#navbar').collapse("toggle");
-        }
-
-    });
-    
-
-    $('body').scrollspy({ target: '#main-nav' });
-    
-
-    function changeMenuColorAndOpacity() {
-        if (window.scrollY >= 100) {
-            $("#main-nav").css({ "background": "#fff"});
-            $("#main-nav .navbar-nav>li>a").css({color: "#777"});
-            $("#main-nav .navbar-nav>li>a:hover").css({color: "#0ed8d6;"});
-
-        } else if (window.scrollY < 100 && window.innerWidth <= 768) {
-            
-            $("#main-nav").css({ "background": "#fff"});
-            $("#main-nav .navbar-nav>li>a").css({color: "#777"});
-            $("#main-nav .navbar-nav>li>a:hover").css({color: "#0ed8d6;"});
-
-        } else {
-            $("#main-nav").css({ "background": "none"});
-            $("#main-nav .navbar-nav>li>a").css({color: "#fff"});
-
-        }
-    };
-    
-    changeMenuColorAndOpacity();
-    
-    $(window).scroll(function() {
-        changeMenuColorAndOpacity();
-    });
-    
-    $(window).resize(function() {
-        changeMenuColorAndOpacity();
-    });
-    
-});
-
-$(document).on('click', 'a[href^="#"]', function(event){
-		event.preventDefault();
-		
-		$('html, body').animate({
-			scrollTop: $( $.attr(this, 'href') 
-			).offset().top - 91}, 500);
-		})
+	
+	if($(window).scrollTop() >= 50){
+		$('#main-nav').addClass('scroll');
+	}
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() >= 50 ) {
+			$('#main-nav').addClass('scroll');
+			$('#main-nav').css({'transition':'all 0.3s'})
+		} else {
+			$('#main-nav').removeClass('scroll');
+		}
 	});
 	
+});
